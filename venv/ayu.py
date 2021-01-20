e6c5984c7a86ee2e8107e51c7c7fd2e102a59194
ayush-srivasta <ayushsrivastav1999@gmail.com>
2021-01-20 22:23:33 +0530

    Calcultor function added

--- venv/ayu.py
+++ venv/ayu.py
@@ -8,7 +8,7 @@ from gtts import gTTS as gt
 import threading
 from datetime import datetime
 import subprocess
-
+import os
 
  #it takes input from micrphone and convert it in text form and return
 def listen():
@@ -21,32 +21,45 @@ def listen():
         text=pr.recognize_google(audio,language="en-in")
     return text
 # This function will decide which function to call on the basis of the user command
-def functionality():         # kuch bhi
-    text=listen().split()
-    fin_text=""
-    print(text)
-    option=text[0]
-    for i in range(1,len(text)):
-        fin_text+=text[i]
-        fin_text+=" "
-    print(fin_text)
-    if(option=="Play" or option=="play"):
-        play_audio(fin_text)
-    elif(option=='Who' or option=='who' or option=='What' or option=='what'):
-        wiki(fin_text)
-    elif('browser' in fin_text):
-        open_browser()
-    elif(option=='Set' or option=='set'):
-        reminder()
-    elif(option=='Yes' or option=='yes'):
-        functionality()
-    elif(option=='No' or option=='no'):
-        exit(0)
-    elif 'calculator' in fin_text:
-        calculator()
-    elif 'Notepad' in fin_text:
-        openNodepad()
-
+def functionality():
+   while True:
+        open_msg="What you want me to do"
+        audio = gt(text=open_msg, lang='en', slow=False)
+        audio.save("start_sound.mp3")
+        playsound('start_sound.mp3')
+        os.remove("start_sound.mp3")
+        text=listen().split()
+        fin_text=""
+        print(text)
+        option=text[0]
+        for i in range(1,len(text)):
+            fin_text+=text[i]
+            fin_text+=" "
+        print(fin_text)
+        if(option=="Play" or option=="play"):
+            play_audio(fin_text)
+        elif(option=='Who' or option=='who' or option=='What' or option=='what'):
+            wiki(fin_text)
+        elif('browser' in fin_text):
+            open_browser()
+        elif(option=='Set' or option=='set'):
+            reminder()
+    # elif(option=='Yes' or option=='yes'):
+    #     functionality()
+    # elif(option=='No' or option=='no'):
+    #     exit(0)
+        elif 'calculator' in fin_text:
+            calculator()
+        elif 'Notepad' in fin_text:
+            openNodepad()
+        if to_repeat():
+            continue
+        else:
+            speek="Program is terminating"
+            audio = gt(text=speek, lang='en', slow=False)
+            audio.save("last_voice.mp3")
+            playsound('last_voice.mp3')
+            break
 
 
 
@@ -104,6 +117,7 @@ def reminder():
     audio = gt(text=temp, lang='en', slow=False)
     audio.save("summ.mp3")
     playsound('summ.mp3')
+    os.remove('summ.mp3')
     timer=threading.Timer(diffrence,alarm_sound)
     timer.start()
 
@@ -117,17 +131,30 @@ def openNodepad():
     subprocess.Popen("C:\\Windows\\System32\\notepad.exe")
 # openNodepad()
 #calculator()
-functionality()
+def to_repeat():
+    temp="You want to continue"
+    audio = gt(text=temp, lang='en', slow=False)
+    audio.save("to_repeat_sound.mp3")
+    playsound('to_repeat_sound.mp3')
+    os.remove("to_repeat_sound.mp3")
+    text=listen()
+    print(text)
+    if text=="yes" or text=="Yes":
+        return True
+    else:
+        return False
+
+
+
+
+if __name__ == "__main__":
+    functionality()
+
+
 # temp = "Want to quit or do something more"
 # audio = gt(text=temp, lang='en', slow=False)
 # audio.save("su.mp3")
 # playsound('su.mp3')
-def to_repeat():
-    temp="You want to quit or want to continue"
-    audio = gt(text=temp, lang='en', slow=False)
-    audio.save("to_repeat_sound.mp3")
-    playsound('to_repeat_sound.mp3')
-
 
 
 
